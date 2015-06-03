521561c19a2179ffe4b7046b1773d8f99f7c27a9
Mitch Downey <mitchldowney@gmail.com>
2015-06-02 20:36:31 -0500

    Initial commit for Podverse rebuild

---
+++ podverse/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  podverse
+//
+//  Created by Mitchell Downey on 6/2/15.
+//  Copyright (c) 2015 Mitchell Downey. All rights reserved.
+//
+
+#ifndef podverse_Bridging_Header_h
+#define podverse_Bridging_Header_h
+
+#import "MWFeedInfo.h"
+#import "MWFeedItem.h"
+#import "MWFeedParser.h"
+
+#endif
