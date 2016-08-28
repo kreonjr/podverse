5cb628127a940b392f56c12469268c5a03a8e7b0
Mitch Downey <mitchldowney@gmail.com>
2016-08-28 05:22:08 -0500

    Begin podcast profile cleanup; separate searchResult from coredataPodcasts

--- podverse/PodcastProfileViewController.swift
+++ podverse/PodcastProfileViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 
 class PodcastProfileViewController: UIViewController {
-        
-    var searchResultPodcast: SearchResultPodcast!
+    
+    
+    
+    var searchResultPodcast: SearchResultPodcast?
+    var podcast: Podcast?
     
     @IBOutlet weak var searchResultImage: UIImageView!
     @IBOutlet weak var searchResultTitle: UILabel!
     @IBOutlet weak var searchResultTotalClips: UILabel!
     @IBOutlet weak var searchResultLastPublishedDate: UILabel!
-    
-    @IBOutlet weak var searchResultHeaderView: UIView!
     
     @IBOutlet weak var searchResultPrimaryGenreName: UILabel!
     @IBOutlet weak var searchResultEpisodesTotal: UILabel!
@@ -25,45 +26,94 @@ class PodcastProfileViewController: UIViewController {
     
     @IBOutlet weak var searchResultSummary: UITextView!
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.rightBarButtonItem = self.playerNavButton()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchResultHeaderView.layer.borderColor = UIColor.lightGrayColor().CGColor
-        searchResultHeaderView.layer.borderWidth = 0.5
-        
-        searchResultTitle.text = searchResultPodcast.title
-        
-        if let imageUrlString = searchResultPodcast.imageURL, let imageUrl = NSURL(string: imageUrlString) {
-            UIImage.downloadImageWithURL(imageUrl, completion: { (completed, image) -> () in
-                if completed {
-                   self.searchResultImage?.image = image
-                }
-                else {
-                    if let itunesUrlString = self.searchResultPodcast.itunesImageURL, let itunesUrl = NSURL(string: itunesUrlString) {
-                        UIImage.downloadImageWithURL(itunesUrl, completion: { (completed, iTunesImage) -> () in
-                            if completed {
-                                self.searchResultImage?.image = iTunesImage
-                            }
-                        })
+        if let searchPodcast = searchResultPodcast {
+            searchResultTitle.text = searchPodcast.title
+            
+            searchResultTotalClips.text = "123 clips"
+            
+            if let lastPubDate = searchPodcast.lastPubDate {
+                searchResultLastPublishedDate.text = PVUtility.formatDateToString(lastPubDate)
+            }
+            
+            if let genre = searchPodcast.primaryGenreName {
+                searchResultPrimaryGenreName.text = "Genre: " + genre
+            }
+
+            //
+            ////        searchResultEpisodesTotal.text = "Episodes Available: " + String(searchResultPodcast.episodesTotal!)
+            
+            //        searchResultFeedURL.text = "RSS Feed: " + searchResultPodcast.feedURL
+            
+            if let artistName = searchPodcast.artistName {
+                searchResultSummary.text = artistName
+            }
+            
+            if let imageUrlString = searchPodcast.imageURL, let imageUrl = NSURL(string: imageUrlString) {
+                UIImage.downloadImageWithURL(imageUrl, completion: { [weak self] (completed, image) -> () in
+                    if completed {
+                        self?.searchResultImage?.image = image
                     }
-                }
-            })
+                    else {
+                        if let itunesUrlString = searchPodcast.itunesImageURL, let itunesUrl = NSURL(string: itunesUrlString) {
+                            UIImage.downloadImageWithURL(itunesUrl, completion: { [weak self] (completed, iTunesImage) -> () in
+                                if completed {
+                                    self?.searchResultImage?.image = iTunesImage
+                                }
+                            })
+                        }
+                    }
+                })
+            }
+        } else if let coredataPodcast = podcast {
+            searchResultTitle.text = coredataPodcast.title
+            
+            searchResultTotalClips.text = "123 clips"
+            
+            if let lastPubDate = coredataPodcast.lastPubDate {
+                searchResultLastPublishedDate.text = PVUtility.formatDateToString(lastPubDate)
+            }
+            
+//            if let genre = coredataPodcast.primaryGenreName {
+//                searchResultPrimaryGenreName.text = "Genre: " + genre
+//            }
+            
+            //
+            ////        searchResultEpisodesTotal.text = "Episodes Available: " + String(searchResultPodcast.episodesTotal!)
+            
+            //        searchResultFeedURL.text = "RSS Feed: " + searchResultPodcast.feedURL
+            
+            if let author = coredataPodcast.author {
+                searchResultSummary.text = author
+            }
+            
+            if let imageUrlString = coredataPodcast.imageURL, let imageUrl = NSURL(string: imageUrlString) {
+                UIImage.downloadImageWithURL(imageUrl, completion: { [weak self] (completed, image) -> () in
+                    if completed {
+                        self?.searchResultImage?.image = image
+                    }
+                    else {
+                        if let itunesUrlString = coredataPodcast.itunesImageURL, let itunesUrl = NSURL(string: itunesUrlString) {
+                            UIImage.downloadImageWithURL(itunesUrl, completion: { [weak self] (completed, iTunesImage) -> () in
+                                if completed {
+                                    self?.searchResultImage?.image = iTunesImage
+                                }
+                            })
+                        }
+                    }
+                })
+            }
+
         }
         
-        searchResultTotalClips.text = "123 clips"
-        
-        searchResultLastPublishedDate.text = PVUtility.formatDateToString(searchResultPodcast.lastPubDate!)
-
-        searchResultPrimaryGenreName.text = "Genre: " + searchResultPodcast.primaryGenreName!
-//
-////        searchResultEpisodesTotal.text = "Episodes Available: " + String(searchResultPodcast.episodesTotal!)
-        
-//        searchResultFeedURL.text = "RSS Feed: " + searchResultPodcast.feedURL
-
-        searchResultSummary.text = searchResultPodcast.artistName!
-        
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -71,15 +121,12 @@ class PodcastProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == Constants.TO_PLAYER_SEGUE_ID {
+            let mediaPlayerViewController = segue.destinationViewController as! MediaPlayerViewController
+            mediaPlayerViewController.hidesBottomBarWhenPushed = true
+        }
     }
-    */
 
 }
